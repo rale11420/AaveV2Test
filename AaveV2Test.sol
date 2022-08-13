835b7e7ee4b48d250d37a98b68710914d238caaa
rale11420 <44586074+rale11420@users.noreply.github.com>
2022-08-13 22:42:55 +0200

    Update AaveV2Test.sol

--- AaveV2Test.sol
+++ AaveV2Test.sol
@@ -14,7 +14,7 @@ contract AaveV2Test {
 
     //modifiers
     modifier onlyLenders {
-        require(amountPleged[msg.sender] > 0, "Invalid address");
+        require(amountPledged[msg.sender] > 0, "Invalid address");
         _;
     }
 
@@ -28,24 +28,28 @@ contract AaveV2Test {
     uint total;
     //totalAdded used for calculating amount to withdraw
     uint totalAdded;
-    mapping(address => uint) public amountPleged;
+    mapping(address => uint) public amountPledged;
     mapping(address => uint) public donationTimeStamp;
     mapping(address => uint) public locks;
 
+    /// @notice setting timelock and getting needed contracts
+    /// @param _timeLock is minimum time needed to pass if lender wants full amount pledged
     constructor(uint _timeLock) {
-        //provider is used for getting lending pool address, because it is upgradeable
-        //addresses shouldn't be hardcoded, remix used to create contract
-        //in hardhat i'll use .env to set addresses there
+        //provider is used for getting lending pool address
+        //addresses shouldn't be hardcoded
         WETHGateway = IWETHGateway(address(0xA61ca04DF33B72b235a8A28CfB535bb7A5271B70));
         provider = ILendingPoolAddressesProvider(address(0x88757f2f99175387aB4C6a4b3067c77A695b0349));
+        //getting lending pool
         LendingPool = ILendingPool(provider.getLendingPool());
+        //getting aWETH contract
         aWETH = IERC20(0x87b1f4cf9BD63f7BBD3eE1aD04E8F52540349347);  
 
         timeLock = _timeLock;     
     }   
 
+    /// @notice Function for adding ETH in Aave lending pool
     function addFunds() external payable {
-        require(msg.value > 0, "Invalid pleged amount");
+        require(msg.value > 0, "Invalid pledged amount");
         if(msg.sender == address(0)){
             revert InvalidAddress();
         }
@@ -53,19 +57,21 @@ contract AaveV2Test {
         uint amount = msg.value;
         address caller = msg.sender;
 
-        amountPleged[caller] += amount;
+        amountPledged[caller] += amount;
         donationTimeStamp[caller] = block.timestamp;
         locks[msg.sender] = block.timestamp;
         totalAdded += amount;
         total += amount;
-
+        
+        //deposit ETH to Aave lending pool
         WETHGateway.depositETH{value: msg.value}(address(LendingPool), address(this), 0);
 
         emit Deposit(caller, amount);
     }  
 
+    /// @notice Function to withdraw funds from Aave lending pool
     function withdraw() external onlyLenders {
-        require(amountPleged[msg.sender] > 0, "Zero funds");
+        require(amountPledged[msg.sender] > 0, "Zero funds");
         if(msg.sender == address(0)){
             revert InvalidAddress();
         }
@@ -74,13 +80,14 @@ contract AaveV2Test {
         WETHGateway.withdrawETH(address(LendingPool), amountToWithdraw, msg.sender);
 
         total -= amountToWithdraw;
-        totalAdded -= amountPleged[msg.sender];
+        totalAdded -= amountPledged[msg.sender];
 
-        amountPleged[msg.sender] = 0;
+        amountPledged[msg.sender] = 0;
         
         emit Withdraw(msg.sender, amountToWithdraw);
     }   
 
+    /// @notice Functions enables lenders to withdraw interest from Aave
     function getReward() external onlyLenders {
         uint amountToWithdraw;
 
@@ -91,7 +98,8 @@ contract AaveV2Test {
         }
 
         WETHGateway.withdrawETH(address(LendingPool), amountToWithdraw, msg.sender);
-
+    
+        emit GetReward(msg.sender, amountToWithdraw);
     }
 
     //getters
@@ -103,29 +111,29 @@ contract AaveV2Test {
     //gets reward amount 
     function getRewardAmount(address account) public view returns(uint reward) {
         uint totalInContract = getTotal();
-        uint pleged = total;
-        uint usersPleged = amountPleged[account];
+        uint pledged = total;
+        uint usersPledged = amountPledged[account];
 
-        //calculate total rewards (totalInContract - pleged)
-        //get user share (usersPleged / pleged)
-        reward = (totalInContract - pleged)*(usersPleged / pleged);         
+        //calculate total rewards (totalInContract - pledged)
+        //get user share (usersPledged / pledged)
+        reward = (totalInContract - pledged)*(usersPledged / pledged);         
     }
 
     //gets full or share of the deposit amount
     function getAmountToWithdraw(address account) public view returns(uint amount) {
-        uint plegedTime = donationTimeStamp[account];
-        uint pleged = total*(amountPleged[msg.sender] / (totalAdded));
+        uint pledgedTime = donationTimeStamp[account];
+        uint pledged = (total * amountPledged[account]) / (totalAdded);
 
-        if (block.timestamp >= plegedTime + timeLock) {
+        if (block.timestamp >= pledgedTime + timeLock) {
             //if locktime passed user can withdraw total amount
-            //(pleged / totalAdded) used for getting share of total amount 
-            amount = total * (pleged / totalAdded);
+            //(pledged / totalAdded) used for getting share of total amount 
+            amount = total * (pledged / totalAdded);
         } else {
-            uint timePassed = block.timestamp - plegedTime;
-            uint amountPerSec = pleged / timeLock;
+            uint timePassed = block.timestamp - pledgedTime;
+            uint amountPerSec = pledged / timeLock;
 
             //amount to withdraw is 50% + timePassed*amountPerSec
-            amount = (pleged + timePassed * amountPerSec) / 2;
+            amount = (pledged + timePassed * amountPerSec) / 2;
         }
     }
 }
